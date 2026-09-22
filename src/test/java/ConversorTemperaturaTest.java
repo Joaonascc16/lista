f70b6da5bf85a64c3fd26c99721cb8b00c227782
Joaonascc <donascimentomoraisj@gmail.com>
2026-09-22 20:58:24 -0300

    Atualizar testes

---
+++ src/test/java/ConversorTemperaturaTest.java
@@ -0,0 +1,25 @@
+import org.example.ConversorTemperatura;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ConversorTemperaturaTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "0, 32",
+            "10, 50",
+            "20, 68",
+            "30, 86",
+            "100, 212",
+            "-10, 14",
+            "-40, -40"
+    })
+    void deveConverterCelsiusParaFahrenheit(double celsius, double esperado) {
+
+        double resultado = ConversorTemperatura.celsiusParaFahrenheit(celsius);
+
+        assertEquals(esperado, resultado, 0.001);
+    }
+}
